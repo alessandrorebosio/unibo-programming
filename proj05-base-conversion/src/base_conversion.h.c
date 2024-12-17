f74a981287c424cac6ec81a350c32000542e9719
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2024-12-17 13:34:01 +0100

    completed proj05-base-comversion

--- proj05-base-conversion/src/base_conversion.h.c
+++ proj05-base-conversion/src/base_conversion.h.c
@@ -1,11 +1,34 @@
 #include "base_conversion.h"
+#include <stdio.h>
 
 void base_conversion_rc(unsigned int n, unsigned int b)
 {
-    return;
+    if (n == 0)
+        return;
+
+    base_conversion_rc(n / b, b);
+
+    if (n % b < 10)
+        printf("%d", n % b);
+    else
+        printf("%c", 'A' + (n % b) - 10);
 }
 
 void base_conversion_it(unsigned int n, unsigned int b)
 {
-    return;
+    unsigned int potenza = 1;
+
+    while (n / potenza >= b)
+        potenza *= b;
+
+    while (potenza > 0)
+    {
+        if (n / potenza < 10)
+            printf("%c", n / potenza + '0');
+        else
+            printf("%c", 'A' + n / potenza - 10);
+
+        n %= potenza;
+        potenza /= b;
+    }
 }
