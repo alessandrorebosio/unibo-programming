dc66feebf437649c03a7fde915de92675abd4aa3
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2024-12-17 13:28:36 +0100

    completed proj00-test

--- proj00-test/src/main.c
+++ proj00-test/src/main.c
@@ -3,5 +3,18 @@
 
 int main()
 {
+    /*
+        store the first name
+        store the last name
+        store the student ID number
+
+        print information
+    */
+    char name[] = "Alessandro";
+    char surname[] = "Rebosio";
+    int mat = 1130557;
+
+    printf("NAME: %-10s COGNOME: %-10s MATRICOLA: %010d", name, surname, mat);
+
     return EXIT_SUCCESS;
 }
