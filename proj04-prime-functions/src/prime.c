8f020153a9dcb4f5de2cc2fa3e1b451e4a484326
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2024-12-17 13:32:39 +0100

    completed proj04-prime-functions

--- proj04-prime-functions/src/prime.c
+++ proj04-prime-functions/src/prime.c
@@ -4,20 +4,51 @@
 
 unsigned short int is_prime(unsigned short int n)
 {
-    return 0;
+    int i;
+    for (i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return 0;
+    return 1;
 }
 
 unsigned short int nth_prime(unsigned short int n)
 {
+    int i = 0, k = 2;
+    while (i <= n)
+    {
+        if (is_prime(k))
+        {
+            if (i == n && k <= USHRT_MAX)
+                return k;
+            ++i;
+        }
+        ++k;
+    }
     return 0;
 }
 
 unsigned short int succ_prime(int reset)
 {
+    static int n = 1;
+    if (reset != 0 || n >= USHRT_MAX)
+        n = 1;
+
+    while (n < USHRT_MAX)
+        if (is_prime(++n))
+            return n;
+
     return 0;
 }
 
 unsigned short int co_prime(unsigned short int m, unsigned short int n)
 {
+    int min = m > n ? n : m, i = 0;
+
+    for (i = 2; i <= min; i++)
+    {
+        if (n % i == 0 && m % i == 0)
+            return 0;
+    }
+
     return 1;
 }
